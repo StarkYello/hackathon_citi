19221717e2270fe1b12083873436ce852b31ed3e
Xing <rendykesto@yahoo.com>
2018-08-02 14:09:07 -0500

    Initial commit

---
+++ app/src/main/java/com/intern/xing/hackathon_citi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.intern.xing.hackathon_citi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
