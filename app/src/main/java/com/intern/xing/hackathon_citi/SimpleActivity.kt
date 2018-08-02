050b67f351eb416a71e3ea56d4bf8d054bd4d46a
Xing <rendykesto@yahoo.com>
2018-08-02 16:44:39 -0500

    first screen is done

--- app/src/main/java/com/intern/xing/hackathon_citi/SimpleActivity.kt
+++ app/src/main/java/com/intern/xing/hackathon_citi/SimpleActivity.kt
@@ -1,5 +1,6 @@
 package com.intern.xing.hackathon_citi
 
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,10 @@ class SimpleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.gold)))
+        supportActionBar!!.title = "Accounts"
+//        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(resources.getDrawable(R.drawable.sandwich))
     }
 }
