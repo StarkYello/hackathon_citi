5d32321bdec76e1babec7c421ea90d9c5fa019c4
Xing <rendykesto@yahoo.com>
2018-08-03 10:13:07 -0500

    second screen is done

--- app/src/main/java/com/intern/xing/hackathon_citi/SpendingActivity.kt
+++ app/src/main/java/com/intern/xing/hackathon_citi/SpendingActivity.kt
@@ -8,5 +8,7 @@ class SpendingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spending)
+        supportActionBar!!.title = "Your Spending"
+
     }
 }
