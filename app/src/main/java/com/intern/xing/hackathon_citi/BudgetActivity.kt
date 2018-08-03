13319590413bebcd37bdb44be146c0d42136336b
Xing <rendykesto@yahoo.com>
2018-08-02 21:20:33 -0500

    second screen is done

--- app/src/main/java/com/intern/xing/hackathon_citi/BudgetActivity.kt
+++ app/src/main/java/com/intern/xing/hackathon_citi/BudgetActivity.kt
@@ -1,8 +1,10 @@
 package com.intern.xing.hackathon_citi
 
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_budget.*
 
 class BudgetActivity : AppCompatActivity() {
 
@@ -11,5 +13,19 @@ class BudgetActivity : AppCompatActivity() {
         setContentView(R.layout.activity_budget)
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.gold)))
         supportActionBar!!.title = "Budget Screen"
+        arrowButton.setOnClickListener {
+            startActivity(Intent(this,SpendingActivity::class.java))
+        }
+        bottom_navigation.setOnNavigationItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.homeScreen -> {
+                    startActivity(Intent(this, BudgetActivity::class.java))
+                }
+                R.id.experienceScreen -> {
+                    startActivity(Intent(this, ExperienceActivity::class.java))
+                }
+            }
+            false
+        }
     }
 }
